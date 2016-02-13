01197874fbe8b98224dfa8a2003496218e022c0e
Rohan Nagar <rohan.nagar@utexas.edu>
2016-02-13 17:34:26 -0600

    Update email to send correct information

--- src/com/ee461l/blog/EmailCronServlet.java
+++ src/com/ee461l/blog/EmailCronServlet.java
@@ -1,6 +1,9 @@
 package com.ee461l.blog;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
 import java.util.Properties;
 import java.util.List;
 
@@ -21,32 +24,60 @@ public class EmailCronServlet extends HttpServlet {
         ObjectifyService.register(Subscriber.class);
     }
 	
+	public final static long MILLIS_PER_DAY = 24 * 60 * 60 * 1000L;
+	
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		List<BlogPost> posts = ObjectifyService.ofy().load().type(BlogPost.class).list();   
+		Collections.sort(posts);
+		
+		Long now = new Date().getTime();
+		List<BlogPost> postsToSend = new ArrayList<>();
+		
+		for (BlogPost p : posts) {
+			Long postTime = p.getDate().getTime();
+			if (now - postTime <= MILLIS_PER_DAY) {
+				postsToSend.add(p);
+			}
+		}
+		
 		List<Subscriber> subscribers = ObjectifyService.ofy().load().type(Subscriber.class).list();
 		
 		Properties prop = new Properties();
 		Session session = Session.getDefaultInstance(prop, null);
 
-        String msgBody = "TEST MESSAGE!";
-		for (Subscriber s : subscribers) {
-			try {
-	            Message msg = new MimeMessage(session);
-	            msg.setFrom(new InternetAddress("anything@polished-scope-122022.appspotmail.com", "Rohan & Ashar's Blog"));
-	            msg.addRecipient(Message.RecipientType.TO,
-	                             new InternetAddress(s.getEmail(), s.getUser().getNickname()));
-	            msg.setSubject("A TEST MESSAGE.");
-	            msg.setText(msgBody);
-	            Transport.send(msg);
-
-	        } catch (AddressException e) {
-	            // ...
-	        } catch (MessagingException e) {
-	            // ...
-	        }
+		if (!postsToSend.isEmpty()) {
+	        String msgBody = buildMessage(postsToSend);
+			for (Subscriber s : subscribers) {
+				try {
+		            Message msg = new MimeMessage(session);
+		            msg.setFrom(new InternetAddress("anything@polished-scope-122022.appspotmail.com", "Best Blog"));
+		            msg.addRecipient(Message.RecipientType.TO,
+		                             new InternetAddress(s.getEmail(), s.getUser().getNickname()));
+		            msg.setSubject("Best Blog 24 Hour Updates");
+		            msg.setText(msgBody);
+		            Transport.send(msg);
+	
+		        } catch (AddressException e) {
+		            // ...
+		        } catch (MessagingException e) {
+		            // ...
+		        }
+			}
 		}
 	}
 	
 	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		doGet(req, resp);
+	}
+	
+	private String buildMessage(List<BlogPost> posts) {
+		StringBuilder s = new StringBuilder();
+		s.append("New posts in the last 24 hours:\n\n");
+		
+		for (BlogPost p : posts) {
+			s.append(p.getTitle() + "\n" + p.getUser().getNickname() + " " + p.getDate() + "\n" + p.getContent() + "\n\n");
+		}
+		
+		return s.toString();
 	}
 }
