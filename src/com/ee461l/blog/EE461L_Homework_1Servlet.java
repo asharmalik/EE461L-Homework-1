d00a7f72ec9e15e1f07376bee47cc151463c3f9b
Rohan Nagar <rohan.nagar@utexas.edu>
2016-02-13 13:49:14 -0600

    Initalize project

---
+++ src/com/ee461l/blog/EE461L_Homework_1Servlet.java
@@ -0,0 +1,12 @@
+package com.ee461l.blog;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class EE461L_Homework_1Servlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
